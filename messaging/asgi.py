026c5eeac323fbe12d0f7f808c5b94aa7803f312
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2021-08-24 23:12:57 +0100

    initial commit

--- messaging/asgi.py
+++ messaging/asgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter
+import django
+from channels.http import AsgiHandler
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging.settings')
+django.setup()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": AsgiHandler(),
+  # Just HTTP for now. (We can add other protocols later.)
+})
