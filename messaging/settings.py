d8aef01025801461f0ce3f44a8ab91e58097b7fa
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2021-09-01 19:56:44 +0100

    added django environ

--- messaging/settings.py
+++ messaging/settings.py
@@ -13,15 +13,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@t=4evf08geo&wmeoae+w*!t9w1)&7n&+@w^!r_2+z1+2x1h12'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
